def217d021e4b8c6d6dbda3b759c9210997ded24
param7500 <73216269+param7500@users.noreply.github.com>
2020-10-29 10:57:10 +0530

    go to

--- cprogram.c
+++ cprogram.c
@@ -2,4 +2,35 @@
 int main()
 { printf("hello");
 return 0;
+
+void main()
+{
+    
+    int age;
+   
+   l1: 
+    printf("\nEnter any age : ");
+    scanf("%d",&age);
+    
+    if(age>=18)
+    {
+        printf("\nUser can vote");
+        
+    }
+    
+    else if(age == 0){
+        printf("\nNo result found");
+    
+        goto l2;
+    }
+   
+    else if(age<18){
+        printf("\nuser cannot vote");
+    }
+    
+    goto l1;
+    
+    l2:
+    printf("\nend");
+    
 }
